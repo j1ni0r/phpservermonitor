10f06f564fc4373c45a945cbfd7742758e17766a
j1ni0r <shmjunior@msn.com>
2020-07-18 08:40:55 -0300

    primeiro commit funcionando

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('PSM_DB_HOST', 'localhost');
+define('PSM_DB_PORT', '');
+define('PSM_DB_NAME', 'monitordb3');
+define('PSM_DB_USER', 'root');
+define('PSM_DB_PASS', 'ti1024');
+define('PSM_DB_PREFIX', 'psm_');
+define('PSM_BASE_URL', 'http://192.168.4.153/phpservermon-3.3.2');

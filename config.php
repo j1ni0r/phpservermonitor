ba97f98cc8bf3795ddfebff8e96acbfdc42453a1
Wilson Júnior <j1ni0r@users.noreply.github.com>
2020-07-19 07:40:09 -0300

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('PSM_DB_HOST', 'localhost');
-define('PSM_DB_PORT', '');
-define('PSM_DB_NAME', 'monitordb');
-define('PSM_DB_USER', 'root');
-define('PSM_DB_PASS', 'ti1024');
-define('PSM_DB_PREFIX', 'psm_');
-define('PSM_BASE_URL', 'http://192.168.4.153/monitor');
